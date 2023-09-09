bd293645ae3647a73a6f1f8df33b81e686ab7115
lukaszmagieranc <lukasz.magiera.nc@gmail.com>
2023-09-09 18:15:04 +0200

    Added two algorithms

---
+++ Problems/LoggerRateLimiter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace Problems
+{
+    class LoggerRateLimiter
+    {
+
+        private const int TIMEOUT = 10;
+        private static Dictionary<string, int> _inputMessagesWithExpiryTimes = new Dictionary<string, int>();
+
+        public static void Solution()
+        {
+            Tuple<string, int>[] input = new Tuple<string, int>[] {
+                    new Tuple<string, int>("foo", 1),
+                    new Tuple<string, int>("bar", 2),
+                    new Tuple<string, int>("foo", 3),
+                    new Tuple<string, int>("bar", 8),
+                    new Tuple<string, int>("foo", 10),
+                    new Tuple<string, int>("foo", 11),
+            };
+
+            Console.WriteLine("[");
+            foreach (Tuple<string, int> msg in input)
+            {
+                Console.Write(ShouldPrintMessage(msg.Item2, msg.Item1) + ", ");
+            }
+            Console.WriteLine("]");
+        }
+
+        public static bool ShouldPrintMessage(int timestamp, string message)
+        {
+            if (!_inputMessagesWithExpiryTimes.ContainsKey(message))
+            {
+                _inputMessagesWithExpiryTimes.Add(message, timestamp);
+                return true;
+            }
+            var oldTimestamp = _inputMessagesWithExpiryTimes[message];
+            if (timestamp - oldTimestamp >= TIMEOUT)
+            {
+                _inputMessagesWithExpiryTimes[message] = timestamp;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /* ======= Complexity explained
+
+        */
+    }
+}
---
+++ Problems/RomanNumerals.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Problems
+{
+    class RomanNumerals
+    {
+        public static void Solution()
+        {
+
+        }
+
+        private static Dictionary<string, int> values = new Dictionary<string, int>()
+    {
+        { "I", 1 },
+        { "V", 5 },
+        { "X", 10 },
+        { "L", 50 },
+        { "C", 100 },
+        { "D", 500 },
+        { "M", 1000 },
+        { "IV", 4 },
+        { "IX", 9 },
+        { "XL", 40 },
+        { "XC", 90 },
+        { "CD", 400 },
+        { "CM", 900 }
+    };
+
+        public static int RomanToInt(string romanNumber)
+        {
+            int sum = 0;
+            int i = 0;
+            while (i < romanNumber.Length)
+            {
+                if (i < romanNumber.Length - 1)
+                {
+                    string doubleSymbol = romanNumber.Substring(i, 2);
+                    // Check if this is the length-2 symbol case.
+                    if (values.ContainsKey(doubleSymbol))
+                    {
+                        sum += values[doubleSymbol];
+                        i += 2;
+                        continue;
+                    }
+                }
+                // Otherwise, it must be the length-1 symbol case.
+                string singleSymbol = romanNumber.Substring(i, 1);
+                sum += values[singleSymbol];
+                i += 1;
+            }
+            return sum;
+        }
+
+
+        /* ======= Complexity explained
+            Time: O(1) only because we have max number of roman characters in the question, it'd be O(n) if it was not specified
+            Space: O(1) 
+        */
+    }
+}
