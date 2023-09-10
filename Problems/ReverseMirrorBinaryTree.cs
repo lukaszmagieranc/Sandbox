b3ad3a304c4a79bf9b22352ec88cb135d894667a
lukaszmagieranc <lukasz.magiera.nc@gmail.com>
2023-09-10 10:14:35 +0200

    Reverse or Mirror Binary Tree Added

---
+++ Problems/ReverseMirrorBinaryTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Problems
+{
+    class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class ReverseMirrorBinaryTree
+    {
+        public static void Solution()
+        {
+
+        }
+
+        private static void swapChildren(TreeNode root)
+        {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+        }
+
+        // public TreeNode InvertTreeRecursive(TreeNode root) {
+        //     if (root != null) {
+        //         // Swap children
+        //         swapChildren(root);    
+        //         // Invert Tree for Left node
+        //         InvertTree(root.left);
+        //         // Invert Tree for Right node
+        //         InvertTree(root.right);
+        //     }
+
+        //     return root;
+        // }
+
+        // iterative solution
+
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            if (root != null)
+            {
+                var queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+                TreeNode firstNodeFromTheQueue;
+                while (queue.TryDequeue(out firstNodeFromTheQueue))
+                {
+                    swapChildren(firstNodeFromTheQueue);
+                    if (firstNodeFromTheQueue.left != null) queue.Enqueue(firstNodeFromTheQueue.left);
+                    if (firstNodeFromTheQueue.right != null) queue.Enqueue(firstNodeFromTheQueue.right);
+                }
+            }
+
+            return root;
+        }
+
+
+        /* ======= Complexity explained
+
+        */
+    }
+}
